92e18fea2d8161fc44e08595ff4f4557a0640c1d
Justin Allen <97097343+Justin7624@users.noreply.github.com>
2026-04-19 15:48:28 -0400

    Adjust price multipliers for various products

--- app_fixed.js
+++ app_fixed.js
@@ -156,11 +156,11 @@ let sortKey = store.get('bvs.priceSortKey', 'name');
 let sortDir = store.get('bvs.priceSortDir', 'asc');
 
 // Price inflation for everything else
-const DEFAULT_MULTIPLIER = 2; // 
-//Reta, Tirz, and HGH specific.
+const DEFAULT_MULTIPLIER = 1.75; // 
+//Reta, Tirz, cjc+ipa, and HGH specific.
 const PRICE_MULTIPLIER = 4.29;
 // Kit-specific multiplier
-const KIT_MULTIPLIER = 1.75;
+const KIT_MULTIPLIER = 1.50;
 
 const NO_INCREASE = new Set([
   "bacteriostatic water",
